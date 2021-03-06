658b92adb8c22453e0807ffe84c6f892111601fb
JaeHo1234 <mocharete@naver.com>
2021-03-06 17:53:28 +0900

    naver map

--- App.js
+++ App.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, Pressable, Dimensions } from "react-native";
 import Geolocation from 'react-native-geolocation-service';
 import { RectButton } from 'react-native-gesture-handler';
 import NaverMapView, {Circle, Marker, Path, Polyline, Polygon} from "react-native-nmap";
 
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
-function MyMap() {
-    const P0 = {latitude: 37.564362, longitude: 126.977011};
-    const P1 = {latitude: 37.565051, longitude: 126.978567};
-    const P2 = {latitude: 37.565383, longitude: 126.976292};
+const SearchResultMaps = (props) => {
 
-    return <NaverMapView style={{width: '100%', height: '100%'}}
-                         showsMyLocationButton={true}
-                         center={{...P0, zoom: 16}}
-                         onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-                         onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-                         onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
-                  
-                         >
-                                                                        
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')}/>
-        <Marker coordinate={P1} pinColor="blue" onClick={() => console.warn('onClick! p1')}/>
-        <Marker coordinate={P2} pinColor="red" onClick={() => console.warn('onClick! p2')}/>
-        <Path coordinates={[P0, P1]} onClick={() => console.warn('onClick! path')} width={10}/>
-        <Polyline coordinates={[P1, P2]} onClick={() => console.warn('onClick! polyline')}/>
-        <Circle coordinate={P0} color={"rgba(255,0,0,0.3)"} radius={200} onClick={() => console.warn('onClick! circle')}/>
-        <Polygon coordinates={[P0, P1, P2]} color={`rgba(0, 0, 0, 0.5)`} onClick={() => console.warn('onClick! polygon')}/>
-    </NaverMapView>
+    const P0 = {latitude: 37.384661, longitude: 127.107576};
+
+    return (
+        <View style={{width: '100%', height: '100%',}}>
+            <Pressable style={{
+                width: Dimensions.get('screen').width - 40, 
+                height: 50, 
+                backgroundColor: 'white',
+                borderRadius: 25, 
+                position: 'absolute', 
+                top: 30, 
+                elevation: 100,
+                marginHorizontal: 20,
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'row',
+            }} onPress={() => console.warn('searchBtn clicked')}>
+                
+                <Fontisto name="search" size={25} color={'#f15454'} style={{textAlign: 'left'}}/>
+                <Text>Where are you going?</Text>
+            </Pressable>                       
+            <NaverMapView style={{width: '100%', height: '100%', resizeMode: 'cover'}}
+                            center={{...P0}}
+                            showsMyLocationButton={true}                            
+                            onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+                            onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+                            onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+                            useTextureView={true}
+          
+            >
+
+            <Marker coordinate={{latitude: 37.384661, longitude: 127.107576}} caption={{text: 'home', color: 'black', textSize: 10,}}>    
+            </Marker>                                       
+            </NaverMapView>
+        </View>
+    );
 }
 
-export default MyMap;
+export default SearchResultMaps;
